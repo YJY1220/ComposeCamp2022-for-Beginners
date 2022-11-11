2c8c4384b87f6a2ff1c265ab955df55ea7554ba7
YJY1220 <jiyeyu1220@gmail.com>
2022-11-11 11:19:07 +0900

    immage compotable

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.happybirthday
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,6 +17,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 import androidx.compose.material.Colors
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +28,7 @@ class MainActivity : ComponentActivity() {
             HappyBirthdayTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    BirthdayGreetingWithText(message = "Haappy Birthday Sam!", from = "- from Emma")
+                    BirthdayGreetingWithText(message = "Happy Birthday Sam!", from = "- from Emma")
                 }
             }
         }
@@ -48,7 +52,16 @@ fun BirthdayGreetingWithText(message: String, from: String){
 @Preview(showBackground = false)
 @Composable
 fun HappyBirthdayPreview() {
-        HappyBirthdayTheme {
-           BirthdayGreetingWithText( "Happy Birthday Sam!" , " - from Emma")
-        }
+    HappyBirthdayTheme {
+        BirthdayGreetingWithImage(message = "Happy Birthday Sam!", from = "- from Emma")
     }
+}
+
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+}
